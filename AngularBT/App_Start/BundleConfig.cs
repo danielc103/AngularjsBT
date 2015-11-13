651fb8121451cda470ade7bea8e9aa9531c6fc7a
Daniel Carroll <danielc103@gmail.com>
2015-11-12 19:09:53 -0500

    quick layout of page(NO ANGULAR TAGS ADDED) / created script bundles

--- AngularBT/App_Start/BundleConfig.cs
+++ AngularBT/App_Start/BundleConfig.cs
@@ -26,6 +26,13 @@ namespace AngularBT
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angularjs").Include(
+                "~/Scripts/angular.min.js"));
+            
+            bundles.Add(new StyleBundle("~/bundles/app").Include(
+               "~/Scripts/app/binaryTree.js" ));
+
         }
     }
 }
